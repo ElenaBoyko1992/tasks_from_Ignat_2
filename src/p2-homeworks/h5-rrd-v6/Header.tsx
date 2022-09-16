3e3d4c5c783dab9c22c373287cffadb9c2a1fd97
ElenaBoyko1992 <Bionka@inbox.ru>
2022-09-16 17:04:32 +0300

    16/09/22

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
+
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.header}>
+            <div className={s.navLinks}>
+                {/*add NavLinks*/}
+                <NavLink className={s.link} to={'/pre-junior'}>pre-junior </NavLink>
+
+                <NavLink className={s.link} to={'/junior'}>junior </NavLink>
+
+                <NavLink className={s.link} to={'/junior-plus'}>juniorPlus </NavLink>
+
+            </div>
 
         </div>
     )
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import s from './Error404.module.css'
+import {Navigate, useNavigate} from "react-router-dom";
+import {PATH} from "../Pages";
 
 function Error404() {
+    const navigate = useNavigate()
     return (
-        <div>
+        <div className={s.error}>
             <div>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <button className={s.button} onClick={() => {navigate(-1)}}>x
+            </button>
         </div>
     )
 }
