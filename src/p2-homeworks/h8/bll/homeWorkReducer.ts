9b3fa85b460c3fe17c1ede692d8d2f7e8ded3f90
ElenaBoyko1992 <Bionka@inbox.ru>
2022-11-01 15:01:19 +0300

    01/11/22 (8 homework)

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,33 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type SortActionType = {
+    type: 'sort',
+    payload: 'up' | 'down'
+}
+type CheckActionType = {
+    type: 'check',
+    payload: number
+}
+
+type ActionTypes = SortActionType | CheckActionType
+
+export const homeWorkReducer = (state: UserType[], action: ActionTypes): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
+            let newState = [...state]
+            if (action.payload === 'up') {
+                return newState.sort((a, b) => a.age - b.age)
+            } else if (action.payload === 'down') {
+                return newState.sort((a, b) => b.age - a.age)
+            }
             return state
         }
         case 'check': {
             // need to fix
-            return state
+            return [...state].filter(u => u.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
