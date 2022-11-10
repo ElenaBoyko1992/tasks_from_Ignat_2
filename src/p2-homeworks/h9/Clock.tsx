3b3edb58f63c6701faeb354d3ea11fabc8a6f47c
ElenaBoyko1992 <Bionka@inbox.ru>
2022-11-10 13:27:22 +0300

    10/11/22 (9 homework)

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,47 +1,61 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW9.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
         // stop
+        clearInterval(timerId);
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+    const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+    const seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+    const month = (date.getMonth()+1) < 10 ? '0' + (date.getMonth()+1) : (date.getMonth()+1)
+    const year = date.getFullYear()
+
+    const stringTime = `${hours}:${minutes}:${seconds}` // fix with date
+    const stringDate = `${day}.${month}.${year}` // fix with date
 
     return (
         <div>
             <div
+                className={s.time}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
-
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
-
+            <div className={s.date}>
+                {show && (
+                    <div>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
 
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Clock from './Clock'
+import s from "../h9/HW9.module.css";
 
 function HW9() {
+
     return (
         <div>
             <hr/>
             homeworks 9
-
-            {/*should work (должно работать)*/}
-            <Clock/>
-
+            <div className={s.hw9}>
+                {/*should work (должно работать)*/}
+                <Clock/>
+            </div>
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeClock/>*/}
