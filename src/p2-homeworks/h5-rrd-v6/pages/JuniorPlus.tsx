c33d907c8819656d0a6ecc50583303e2033b17fb
ElenaBoyko1992 <Bionka@inbox.ru>
2023-03-01 18:36:46 +0300

    15 homework (01.03.2023) - not finished

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HW12 from "../../h12/HW12";
 import {HW13} from "../../h13/HW13";
 import HW14 from "../../hw14/HW14";
+import HW15 from "../../hw15/HW15";
 
 function JuniorPlus() {
     return (
@@ -9,6 +10,8 @@ function JuniorPlus() {
             <HW12/>
             <HW13/>
             <HW14/>
+            <HW15/>
+
         </div>
     )
 }
